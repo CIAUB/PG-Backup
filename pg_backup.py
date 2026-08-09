76b124e45cb51d01309c99b8e0807827366a811b
EOAMIR <eoamirm@gmail.com>
2026-08-09 08:06:09 +0330

    Update version number to v3.2 in pg_backup.py

--- pg_backup.py
+++ pg_backup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # ============================================================
-#   Pasarguard Backup Utility  v3.1
+#   Pasarguard Backup Utility  v3.2
 #   Dev by: EOAMIR
 #   GitHub: https://github.com/EOAMIR
 # ============================================================
@@ -150,7 +150,7 @@ def print_header(title=""):
     print()
     for line in LOGO:
         print(center(C.R1 + C.BOLD + line + C.RESET, LOGO_W))
-    sub = C.R1 + C.BOLD + "B A C K U P   U T I L I T Y   v 3 . 1   -   E O A M I R" + C.RESET
+    sub = C.R1 + C.BOLD + "B A C K U P   U T I L I T Y   v 3 . 2   -   E O A M I R" + C.RESET
     print(center(sub, 57))
     print()
     print(hline())
